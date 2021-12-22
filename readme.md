10cbf0965acc83b5f41e6dc3be82fdc0bc32cba8
Tonmoy Deb <tonmoydeb404@gamil.com>
2021-12-22 15:40:09 +0600

    first commit

---
+++ readme.md
@@ -0,0 +1,27 @@
+# React Boilerplate
+
+this is a react boilerplate with linting support. this boilerplate helps you to make a quick **react** project setup. click on <button>Use this template</button> button to create your next project with this template.
+
+### things are used
+
+- Vite JS (frontend tool)
+- EsLint
+- Prettier
+
+give _yarn_ or _npm install_ command to install all required packages.
+
+```javascript
+yarn // npm install
+```
+
+use _lint_ script to install all updated linting packages.
+
+```javascript
+yarn lint // npm run lint
+```
+
+then give _yarn dev_ or _npm run dev_ to start the developer server.
+
+```javascript
+yarn dev // npm run dev
+```
---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import logo from './static/icons/logo.svg';
+import './static/styles/App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Hello Vite + React!</p>
+                <p>
+                    Edit <code>App.jsx</code> and save to test HMR updates.
+                </p>
+                <p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                    {' | '}
+                    <a
+                        className="App-link"
+                        href="https://vitejs.dev/guide/features.html"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Vite Docs
+                    </a>
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
