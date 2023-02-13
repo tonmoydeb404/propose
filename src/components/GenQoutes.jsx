5d6ff8238453dfa9fe5b82ece47650c63596f182
tonmoydeb404 <tonmoydeb404@gmail.com>
2023-02-13 17:02:36 +0600

    image preloader added and qoutes location changed

--- src/components/GenQoutes.jsx
+++ src/components/GenQoutes.jsx
@@ -1,42 +1,34 @@
-import React, { useEffect, useRef } from "react";
-import { Card } from "react-bootstrap";
+import React, { useEffect, useRef } from 'react';
+import { Card } from 'react-bootstrap';
 
-const GenQoutes = ({ className = "", texts = [] }) => {
-  const scrollRef = useRef(null);
+const GenQoutes = ({ className = '', texts = [] }) => {
+    const scrollRef = useRef(null);
 
-  useEffect(() => {
-    if (scrollRef && texts.length) {
-      const postion = scrollRef.current.getBoundingClientRect();
+    useEffect(() => {
+        if (scrollRef && texts.length) {
+            scrollRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+    }, [scrollRef, texts]);
 
-      scrollRef.current.scrollIntoView({ behavior: "smooth" });
-    }
-  }, [scrollRef, texts]);
+    return (
+        <div className={`qoutes ${className}`}>
+            {texts.length
+                ? texts.map((item) => (
+                      <Card
+                          body
+                          key={item.id}
+                          bg={item.id === 'finished' ? 'danger' : 'default'}
+                          text={item.id === 'finished' ? 'light' : 'dark'}
+                          className={`qoutes_card animIn ${item.id === 'finished' ? 'last' : ''}`}
+                      >
+                          <p className={`card-text mb-0 `}>{item.text}</p>
+                      </Card>
+                  ))
+                : ''}
 
-  return (
-    <div className={`qoutes ${className}`}>
-      {texts.length
-        ? texts.map((item) => (
-            <Card
-              body
-              key={item.id}
-              bg={item.id === "finished" ? "danger" : "default"}
-              text={item.id === "finished" ? "light" : "dark"}
-              className={`qoutes_card animIn ${
-                item.id === "finished" ? "last" : ""
-              }`}
-            >
-              <p className={`card-text mb-0 `}>{item.text}</p>
-            </Card>
-          ))
-        : ""}
-
-      <div
-        className="dummyscroll"
-        style={{ float: "left", clear: "both" }}
-        ref={scrollRef}
-      />
-    </div>
-  );
+            <div className="dummyscroll" style={{ float: 'left', clear: 'both' }} ref={scrollRef} />
+        </div>
+    );
 };
 
 export default GenQoutes;
--- src/pages/Proposal.jsx
+++ src/pages/Proposal.jsx
@@ -1,65 +1,75 @@
-import React, { useEffect, useState } from "react";
-import { Button, Col, Container, Row } from "react-bootstrap";
-import { useParams } from "react-router-dom";
-import GenQoutes from "../components/GenQoutes";
-import { propose, qoutes } from "../data.json";
-import { genRandom } from "../utills";
+import React, { useEffect, useState } from 'react';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+import GenQoutes from '../components/GenQoutes';
+import { images } from '../data/preload-image-list.json';
+import { propose, qoutes } from '../data/quotes.json';
+import { genRandom } from '../utills';
 
-const Proposal = ({ className = "" }) => {
-  const { id } = useParams();
-  const person = id.split("-").join(" ");
+const Proposal = ({ className = '' }) => {
+    const { id } = useParams();
+    const person = id.split('-').join(' ');
 
-  const [texts, setTexts] = useState([]);
-  const [currentText, setCurrentText] = useState({
-    image: "/images/image-05.webp",
-    subtext: "I want to tell you something",
-  });
+    const [texts, setTexts] = useState([]);
+    const [currentText, setCurrentText] = useState({
+        image: '/images/image-05.webp',
+        subtext: 'I want to tell you something',
+    });
 
-  const addQoutes = () => {
-    const qoute =
-      texts.length >= qoutes.length ? propose : genRandom(qoutes, texts);
-    setCurrentText((prevData) => ({ ...prevData, ...qoute }));
-    setTexts((prevData) => [...prevData, qoute]);
-  };
+    // add quotes
+    const addQoutes = () => {
+        const qoute = texts.length >= qoutes.length ? propose : genRandom(qoutes, texts);
+        setCurrentText((prevData) => ({ ...prevData, ...qoute }));
+        setTexts((prevData) => [...prevData, qoute]);
+    };
 
-  useEffect(() => {
-    document.title = `${person} - Be My Valentine`;
-  }, [person]);
+    // effects
+    useEffect(() => {
+        document.title = `${person} - Be My Valentine`;
+    }, [person]);
 
-  return (
-    <div
-      className={`proposal ${className}`}
-      style={{
-        "--image": `url(${currentText.image})`,
-      }}
-    >
-      <div className="proposal_media bg-dark d-none d-md-block" />
-      <Container>
-        <Row>
-          <Col md={6} className="ms-auto">
-            <div className="proposal_content py-5">
-              <div className="proposal_header">
-                <h1 className="proposal_title h4">
-                  Hey <b>{person}</b>
-                </h1>
-                <p className="propsal_subtitle">{currentText.subtext}</p>
-              </div>
+    // preload images
+    useEffect(() => {
+        images.forEach((image) => {
+            const img = new Image();
+            img.src = image;
+        });
+    }, []);
 
-              <GenQoutes texts={texts} className="main-content" />
+    return (
+        <div
+            className={`proposal ${className}`}
+            style={{
+                '--image': `url(${currentText.image})`,
+            }}
+        >
+            <div className="proposal_media bg-dark d-none d-md-block" />
+            <Container>
+                <Row>
+                    <Col md={6} className="ms-auto">
+                        <div className="proposal_content py-5">
+                            <div className="proposal_header">
+                                <h1 className="proposal_title h4">
+                                    Hey <b>{person}</b>
+                                </h1>
+                                <p className="propsal_subtitle">{currentText.subtext}</p>
+                            </div>
 
-              {currentText.id !== "finished" ? (
-                <Button variant="danger" onClick={addQoutes}>
-                  {texts.length ? "Next" : "Continue"}
-                </Button>
-              ) : (
-                ""
-              )}
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+                            <GenQoutes texts={texts} className="main-content" />
+
+                            {currentText.id !== 'finished' ? (
+                                <Button variant="danger" onClick={addQoutes}>
+                                    {texts.length ? 'Next' : 'Continue'}
+                                </Button>
+                            ) : (
+                                ''
+                            )}
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
 };
 
 export default Proposal;
