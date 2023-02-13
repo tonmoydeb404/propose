8a5571f2046d0282f24c2f976b25d396819ec99d
tonmoydeb404 <tonmoydeb404@gmail.com>
2023-02-13 16:44:19 +0600

    onclick scroller updated

--- src/components/GenQoutes.jsx
+++ src/components/GenQoutes.jsx
@@ -1,39 +1,42 @@
-import React, { useEffect, useRef } from 'react';
-import { Card } from 'react-bootstrap';
+import React, { useEffect, useRef } from "react";
+import { Card } from "react-bootstrap";
 
-const GenQoutes = ({ className = '', texts = [] }) => {
-    const scrollRef = useRef(null);
+const GenQoutes = ({ className = "", texts = [] }) => {
+  const scrollRef = useRef(null);
 
-    useEffect(() => {
-        if (scrollRef && texts.length) {
-            const postion = scrollRef.current.getBoundingClientRect();
-            if (window.outerHeight < postion.bottom + 50) {
-                scrollRef.current.scrollIntoView({ behavior: 'smooth' });
-            }
-            // console.log('body', window.innerHeight);
-            // console.log('scroll', postion);
-        }
-    }, [scrollRef, texts]);
+  useEffect(() => {
+    if (scrollRef && texts.length) {
+      const postion = scrollRef.current.getBoundingClientRect();
 
-    return (
-        <div className={`qoutes ${className}`}>
-            {texts.length
-                ? texts.map((item) => (
-                      <Card
-                          body
-                          key={item.id}
-                          bg={item.id === 'finished' ? 'danger' : 'default'}
-                          text={item.id === 'finished' ? 'light' : 'dark'}
-                          className={`qoutes_card animIn ${item.id === 'finished' ? 'last' : ''}`}
-                      >
-                          <p className={`card-text mb-0 `}>{item.text}</p>
-                      </Card>
-                  ))
-                : ''}
+      scrollRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [scrollRef, texts]);
 
-            <div className="dummyscroll" style={{ float: 'left', clear: 'both' }} ref={scrollRef} />
-        </div>
-    );
+  return (
+    <div className={`qoutes ${className}`}>
+      {texts.length
+        ? texts.map((item) => (
+            <Card
+              body
+              key={item.id}
+              bg={item.id === "finished" ? "danger" : "default"}
+              text={item.id === "finished" ? "light" : "dark"}
+              className={`qoutes_card animIn ${
+                item.id === "finished" ? "last" : ""
+              }`}
+            >
+              <p className={`card-text mb-0 `}>{item.text}</p>
+            </Card>
+          ))
+        : ""}
+
+      <div
+        className="dummyscroll"
+        style={{ float: "left", clear: "both" }}
+        ref={scrollRef}
+      />
+    </div>
+  );
 };
 
 export default GenQoutes;
