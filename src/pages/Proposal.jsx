f473b184d60adc9a93b9fc712a119200599cc69d
tonmoydeb404 <tonmoydeb404@gmail.com>
2023-02-13 16:44:01 +0600

    image file converted to webp

--- src/pages/Proposal.jsx
+++ src/pages/Proposal.jsx
@@ -1,64 +1,65 @@
-import React, { useEffect, useState } from 'react';
-import { Button, Col, Container, Row } from 'react-bootstrap';
-import { useParams } from 'react-router-dom';
-import GenQoutes from '../components/GenQoutes';
-import { propose, qoutes } from '../data.json';
-import { genRandom } from '../utills';
+import React, { useEffect, useState } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import GenQoutes from "../components/GenQoutes";
+import { propose, qoutes } from "../data.json";
+import { genRandom } from "../utills";
 
-const Proposal = ({ className = '' }) => {
-    const { id } = useParams();
-    const person = id.split('-').join(' ');
+const Proposal = ({ className = "" }) => {
+  const { id } = useParams();
+  const person = id.split("-").join(" ");
 
-    const [texts, setTexts] = useState([]);
-    const [currentText, setCurrentText] = useState({
-        image: '/images/image-05.jpg',
-        subtext: 'I want to tell you something',
-    });
+  const [texts, setTexts] = useState([]);
+  const [currentText, setCurrentText] = useState({
+    image: "/images/image-05.webp",
+    subtext: "I want to tell you something",
+  });
 
-    const addQoutes = () => {
-        const qoute = texts.length >= qoutes.length ? propose : genRandom(qoutes, texts);
-        setCurrentText((prevData) => ({ ...prevData, ...qoute }));
-        setTexts((prevData) => [...prevData, qoute]);
-    };
+  const addQoutes = () => {
+    const qoute =
+      texts.length >= qoutes.length ? propose : genRandom(qoutes, texts);
+    setCurrentText((prevData) => ({ ...prevData, ...qoute }));
+    setTexts((prevData) => [...prevData, qoute]);
+  };
 
-    useEffect(() => {
-        document.title = `${person} - Be My Valentine`;
-    }, [person]);
+  useEffect(() => {
+    document.title = `${person} - Be My Valentine`;
+  }, [person]);
 
-    return (
-        <div
-            className={`proposal ${className}`}
-            style={{
-                '--image': `url(${currentText.image})`,
-            }}
-        >
-            <div className="proposal_media bg-dark d-none d-md-block" />
-            <Container>
-                <Row>
-                    <Col md={6} className="ms-auto">
-                        <div className="proposal_content py-5">
-                            <div className="proposal_header">
-                                <h1 className="proposal_title h4">
-                                    Hey <b>{person}</b>
-                                </h1>
-                                <p className="propsal_subtitle">{currentText.subtext}</p>
-                            </div>
+  return (
+    <div
+      className={`proposal ${className}`}
+      style={{
+        "--image": `url(${currentText.image})`,
+      }}
+    >
+      <div className="proposal_media bg-dark d-none d-md-block" />
+      <Container>
+        <Row>
+          <Col md={6} className="ms-auto">
+            <div className="proposal_content py-5">
+              <div className="proposal_header">
+                <h1 className="proposal_title h4">
+                  Hey <b>{person}</b>
+                </h1>
+                <p className="propsal_subtitle">{currentText.subtext}</p>
+              </div>
 
-                            <GenQoutes texts={texts} className="main-content" />
+              <GenQoutes texts={texts} className="main-content" />
 
-                            {currentText.id !== 'finished' ? (
-                                <Button variant="danger" onClick={addQoutes}>
-                                    {texts.length ? 'Next' : 'Continue'}
-                                </Button>
-                            ) : (
-                                ''
-                            )}
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
+              {currentText.id !== "finished" ? (
+                <Button variant="danger" onClick={addQoutes}>
+                  {texts.length ? "Next" : "Continue"}
+                </Button>
+              ) : (
+                ""
+              )}
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Proposal;
