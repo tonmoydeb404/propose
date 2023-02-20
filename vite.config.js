3deadb574d1568793ebe9796757030e556aadf9f
tonmoydeb404 <tonmoydeb404@gmail.com>
2023-02-21 01:14:16 +0600

    google site verification added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-extraneous-dependencies */
+import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd());
+
+    return {
+        plugins: [
+            react(),
+            createHtmlPlugin({
+                minify: true,
+                inject: { data: { id: env.VITE_APP_GOOGLE_VERIFICATION } },
+            }),
+        ],
+    };
+});
